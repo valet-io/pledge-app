7fc4ec828a1f8c5aa2f6b0e1e9abe7f4995bca4d
Ben Drucker <bvdrucker@gmail.com>
2014-04-14 12:20:23 -0400

    yo angular (closes #11633158818750)

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular
+  .module('valet-io-pledge-app', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
