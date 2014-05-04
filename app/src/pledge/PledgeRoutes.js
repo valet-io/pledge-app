0d4178d386594cdfc35bb072cdc71b338a61922c
Ben Drucker <bvdrucker@gmail.com>
2014-05-04 17:58:30 -0400

    partialify template

--- app/src/pledge/PledgeRoutes.js
+++ app/src/pledge/PledgeRoutes.js
@@ -1,7 +1,7 @@
 module.exports = function ($stateProvider, $urlRouterProvider) {
   $stateProvider.state('pledge', {
     url: '/pledge',
-    template: 'pledge here'
+    template: require('./PledgeForm.html')
   });
   $urlRouterProvider.when('', '/pledge');
 };
