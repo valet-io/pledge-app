0daf4245e4b19083025ee92dc0a4a6887ea4ff52
Ben Drucker <bvdrucker@gmail.com>
2014-05-19 05:24:32 -0400

    set up payment routes

--- app/src/payment/PaymentRoutes.js
+++ app/src/payment/PaymentRoutes.js
@@ -1,3 +1,19 @@
-module.exports = function ($stateProvider) {
+'use strict';
 
+module.exports = function ($stateProvider) {
+  $stateProvider
+    .state('payment', {})
+    .state('payment.create', {
+      url: '/payment/:pledgeId',
+      templateUrl: 'payment/PaymentForm.html',
+      controller: 'PaymentController',
+      resolve: {
+        pledge: function (Pledge, $stateParams) {
+          return new Pledge({
+            id: $stateParams.pledgeId
+          })
+          .fetch();
+        }
+      }
+    });
 };
