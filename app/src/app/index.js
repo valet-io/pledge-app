12ead5b809e51e90cb3a912bce5ff62ca4725892
Ben Drucker <bvdrucker@gmail.com>
2014-06-03 09:12:00 -0400

    set up Raven for sentry (#12772468866921)

--- app/src/app/index.js
+++ app/src/app/index.js
@@ -1,12 +1,18 @@
 'use strict';
 
+require('raven-angular');
+
 require('angular')
   .module('valet-io-pledge-app', [
+    'ngRaven',
     require('../campaign'),
     require('../pledge'),
     require('../donor'),
     require('../payment')
   ])
-  .controller('AppController', require('./AppController'));
+  .controller('AppController', require('./AppController'))
+  .value('RavenConfig', {
+    dsn: 'https://25f8464cfb0b4d63ac7c5840a569382e@app.getsentry.com/25248'
+  });
 
 module.exports = 'PledgeAppModule';
