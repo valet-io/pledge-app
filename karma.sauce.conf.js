d7840b08b4f544150fbcc294e6d633507ea9f2de
Ben Drucker <bvdrucker@gmail.com>
2014-07-02 17:36:10 -0400

    run unit tests on sauce labs

---
+++ karma.sauce.conf.js
@@ -0,0 +1,65 @@
+module.exports = function (config) {
+  var customLaunchers = {
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1',
+    }
+  };
+
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['browserify', 'mocha', 'chai-sinon'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
+      'components/stripe/index.js',
+      'node_modules/angular-ui-router/release/angular-ui-router.js',
+      'test/unit/**/*.js'
+    ],
+
+    preprocessors: {
+      'test/unit/**/*.js': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      transform: ['browserify-shim', 'envify']
+    },
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+    reporters: ['dots', 'saucelabs'],
+
+    sauceLabs: {
+      testName: 'Pledge App Unit Tests',
+      startConnect: false
+    },
+
+    transports: ['xhr-polling'],
+
+    captureTimeout: 0,
+    browserNoActivityTimeout: 0,
+
+    // enable / disable watching file and executing tests whenever any file changes
+
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
