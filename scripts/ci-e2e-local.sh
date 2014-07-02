634302dfcefddc1cd2b43f48f94577693a300ebd
Ben Drucker <bvdrucker@gmail.com>
2014-07-02 10:54:30 -0400

    don't update webdriver

--- scripts/ci-e2e-local.sh
+++ scripts/ci-e2e-local.sh
@@ -1,7 +1,6 @@
 ./node_modules/.bin/gulp build --staging
 ./node_modules/.bin/gulp server & sleep 1
 echo 'Test server online'
-./node_modules/.bin/webdriver-manager update
 ./node_modules/.bin/webdriver-manager start & sleep 5
 echo 'Webdriver online'
 ./node_modules/.bin/protractor
