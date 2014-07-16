1a14612e6661e9f999bf0064a65cae890d3c9ebd
Ben Drucker <bvdrucker@gmail.com>
2014-07-16 08:54:34 -0400

    Display a loading indicator until the active route is fully loaded [Finishes #75107344]

--- src/app/controller.js
+++ src/app/controller.js
@@ -1,3 +1,13 @@
-module.exports = function () {
+'use strict';
 
+module.exports = function ($scope) {
+  $scope.$on('$stateChangeStart', function () {
+    $scope.loaded = false;
+  });
+  $scope.$on('$stateChangeSuccess', function () {
+    $scope.loaded = true;
+  });
+  $scope.$on('$stateChangeError', function () {
+    $scope.loaded = true;
+  });
 };
