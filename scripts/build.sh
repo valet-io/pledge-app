a06273eebb400cd2fa4b6d1fadc298cd4cf1eebf
Ben Drucker <bvdrucker@gmail.com>
2014-07-23 15:59:09 -0400

    Fix typo in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 if [ "${TRAVIS_TAG}" ]; then
   BUILD_ENV="production"
-elif [ "${TRAVIS_BRANCH}" == "master" ]; then
+elif [ "${TRAVIS_BRANCH}" = "master" ]; then
   BUILD_ENV="staging"
 else
   BUILD_ENV="development"
