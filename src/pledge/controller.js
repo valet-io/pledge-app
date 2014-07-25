7f59911e17eb48ad86dbe3217f5e6a07e368730a
Ben Drucker <bvdrucker@gmail.com>
2014-07-25 11:59:06 -0400

    Use the batch api to submit a new pledge + donor

--- src/pledge/controller.js
+++ src/pledge/controller.js
@@ -1,12 +1,32 @@
 'use strict';
 
-module.exports = function ($scope, Pledge, campaign, $state) {
+var angular = require('angular');
+
+module.exports = function ($scope, Pledge, campaign, $state, $http) {
 
   $scope.pledge = new Pledge({campaign_id: campaign.id, anonymous: false}, {withRelated: ['donor']});
 
   $scope.submit = function () {
-    return $scope.pledge.save({
-      withRelated: ['donor']
+    var pledge = angular.copy($scope.pledge);
+    delete pledge.donor;
+    pledge.donor_id = '$$0.id';
+    return $http.post(pledge.baseURL + '/batch', {
+      requests: [
+        {
+          method: 'post',
+          path: '/donors',
+          payload: $scope.pledge.donor
+        },
+        {
+          method: 'post',
+          path: '/pledges',
+          payload: pledge,
+          references: ['payload']
+        }
+      ]
+    })
+    .then(function (res) {
+      return angular.extend($scope.pledge, res.data[1]);
     })
     .then(function (pledge) {
       $state.go('payment', {
