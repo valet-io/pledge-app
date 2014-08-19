9e897531756bf7274beeb8243a61090d8572e1b1
Ben Drucker <bvdrucker@gmail.com>
2014-08-19 15:24:28 -0400

    hack in the pledge/donor relation init and run the batch save serially

--- src/pledge/controllers/create.js
+++ src/pledge/controllers/create.js
@@ -4,14 +4,18 @@ module.exports = function ($scope, $state, Pledge, campaign) {
   $scope.firebase = campaign.listen();
   $scope.campaign = campaign;
   $scope.pledge = new Pledge({campaign_id: campaign.id, anonymous: false}, {expand: ['donor']});
+  
+  // temporary, refactoring convex relations to handle automatically
+  $scope.pledge.donor_id = $scope.pledge.donor.id;
+
   $scope.submit = function () {
     return $scope.pledge.$batch(function (batch) {
+      batch.parallel = false;
       $scope.pledge.donor.$save({batch: batch});
-      $scope.pledge.$save({batch: batch});
-    })
-    .then(function () {
-      $state.go('^.confirmation', {
-        id: $scope.pledge.id
+      $scope.pledge.$save({batch: batch}).then(function () {
+        $state.go('^.confirmation', {
+          id: $scope.pledge.id
+        });
       });
     });
   };
