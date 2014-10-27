98dac04ec048ac75ada82f818165cacf93bc54a7
Ben Drucker <bvdrucker@gmail.com>
2014-10-27 08:55:31 -0400

    Configure e2e to use separate page objects and specs

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,7 @@
 var ci = process.env.CI;
 
 exports.config = {
-  specs: ['test/e2e/*.js'],
+  specs: ['test/e2e/specs/**/*.js'],
   baseUrl: 'http://localhost:8000',
   capabilities: {
     browserName: ci ? 'firefox' : 'chrome'
