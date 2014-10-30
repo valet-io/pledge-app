89f51edbf727abc8134e7b4218577c5480ea2b32
Ben Drucker <bvdrucker@gmail.com>
2014-10-30 07:54:19 -0400

    Add tests for PledgeConfirmationController

--- test/unit/pledge/controllers.js
+++ test/unit/pledge/controllers.js
@@ -4,19 +4,19 @@ var angular = require('angular');
 
 describe('Pledge: Controllers', function () {
 
-  var config, $controller, scope;
+  var config, $controller, scope, $state, sandbox;
   beforeEach(angular.mock.module(require('../../../')));
   beforeEach(angular.mock.inject(function ($injector) {
     config      = $injector.get('config');
     $controller = $injector.get('$controller');
     scope       = $injector.get('$rootScope').$new();
+    $state      = $injector.get('$state');
   }));
 
   describe('PledgeCreateController', function () {
 
-    var $state, Pledge, campaign;
+    var Pledge, campaign;
     beforeEach(angular.mock.inject(function ($injector) {
-      $state   = $injector.get('$state');
       Pledge   = $injector.get('Pledge');
       campaign = new ($injector.get('Campaign'))();
 
@@ -103,6 +103,78 @@ describe('Pledge: Controllers', function () {
           });
           expect(campaign).to.equal(resolved.campaign);
           expect(campaign.$fetch).to.have.been.called;
+        });
+        $timeout.flush();
+      }));
+
+    });
+
+  });
+
+  describe('PledgeConfirmationController', function () {
+
+    var pledge, $timeout;
+    beforeEach(angular.mock.inject(function ($injector) {
+      $timeout = $injector.get('$timeout');
+      $state   = {
+        go: sinon.stub()
+      };
+      pledge = {
+        id: 'id',
+        campaign: {
+          payments: true
+        },
+        $fetch: sinon.stub().returnsThis()
+      };
+    }));
+
+    function invoke () {
+      $controller('PledgeConfirmationController', {
+         $scope: scope,
+         pledge: pledge,
+         $timeout: $timeout,
+         $state: $state
+       });
+    }
+
+    it('publishes the pledges on the scope', function () {
+      invoke();
+      expect(scope.pledge).to.equal(pledge);
+    });
+
+    it('transitions to payment when payments are enabled', function () {
+      invoke();
+      $timeout.flush();
+      expect($state.go).to.have.been.calledWithMatch('payment.create', {
+        pledge: 'id'
+      });
+    });
+
+    it('does not transition when payments are disabled', function () {
+      pledge.campaign.payments = false;
+      invoke();
+      $timeout.flush();
+      expect($state.go).to.not.have.been.called;
+    });
+
+    describe('resolve', function () {
+
+      it('fetches the pledge based on the url', angular.mock.inject(function ($resolve) {
+        var Pledge = sinon.stub().returns(pledge);
+        $resolve.resolve(require('../../../src/pledge/controllers/confirmation').resolve, {
+          Pledge: Pledge,
+          $stateParams: {
+            id: 'id'
+          }
+        })
+        .then(function (resolved) {
+          expect(Pledge).to.have.been.calledWithMatch({
+            id: 'id'
+          });
+          expect(pledge).to.equal(resolved.pledge);
+          expect(pledge.$fetch).to.have.been.calledWithMatch({
+            expand: ['donor', 'campaign']
+          });
         });
         $timeout.flush();
       }));
