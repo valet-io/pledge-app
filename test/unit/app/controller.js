fe987aaf773dc8d381f55d12eaba1c627ce25144
Ben Drucker <bvdrucker@gmail.com>
2014-11-13 09:53:16 -0500

    chore(app): fix test description for fcd8c77

--- test/unit/app/controller.js
+++ test/unit/app/controller.js
@@ -4,7 +4,7 @@ var angular = require('angular');
 
 module.exports = function () {
   
-  it('calls syncLoaded on the scope', angular.mock.inject(function ($controller) {
+  it('calls syncLoaded on the controller', angular.mock.inject(function ($controller) {
     var sync = sinon.spy();
     var controller $controller('AppController', {
       syncLoaded: sync
