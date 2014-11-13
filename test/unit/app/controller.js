2923a65659c4f50dadd3513821e1c3aa85828cdd
Ben Drucker <bvdrucker@gmail.com>
2014-11-13 10:08:21 -0500

    fix(test): Missing equals sign

--- test/unit/app/controller.js
+++ test/unit/app/controller.js
@@ -6,7 +6,7 @@ module.exports = function () {
   
   it('calls syncLoaded on the controller', angular.mock.inject(function ($controller) {
     var sync = sinon.spy();
-    var controller $controller('AppController', {
+    var controller = $controller('AppController', {
       syncLoaded: sync
     });
     expect(sync).to.have.been.calledWith(controller);
